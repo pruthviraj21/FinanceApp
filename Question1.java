4ee3f625c2ae3c3530b982784028d8973c3e727b
pruthviraj21 <113131770+pruthviraj21@users.noreply.github.com>
2023-06-17 13:27:56 +0530

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -6,7 +6,7 @@ public class Question1 {
 	//	message = {74, 97, 118, 97, 32, 105, 115, 32, 103, 114, 101, 97, 116};
 		char msg[]= {(char)74, (char)97,(char) 118, (char)97, (char)32, (char)105, (char)115,(char) 32,(char) 103,(char) 114,(char) 101,(char) 97,(char) 116};
 		for(int i=0;i<msg.length;i++)
-			System.out.print(msg[i]);
+			System.out.print(msg[i]+"4");
 	}
 
 }
